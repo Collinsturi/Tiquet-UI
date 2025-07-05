0b8713238a8c0ce486e7e32b14f9f021a8b278c4
collinsturi <collinsturi>
2025-07-06 00:27:03 +0300

    Feat: Added the General pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Index} from "../src/pages/general/Index.tsx"
+import {Events} from "../src/pages/general/Events.tsx";
+import {EventDetail} from "../src/pages/general/EventDetail.tsx";
+import {HowItWorks} from "../src/pages/general/HowItWorks.tsx";
+import {Contact} from "../src/pages/general/Contact.tsx";
+import {About} from "../src/pages/general/About.tsx";
+import {Legal} from "../src/pages/general/Legal.tsx";
+import Auth from "../src/pages/general/Auth.tsx";
+import {Navigate, Outlet} from "react-router-dom";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+export const AdminRoute = () => {
+  // const user = useSelector((state: RootState) => state.user.user);
+
+  const user = {
+    role: 'admin'
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (user.role !== 'admin') {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+export const EventAttendeeRoute = () => {
+  // const user = useSelector((state: RootState) => state.user.user);
+
+  const user = {
+    role: 'user'
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (user.role !== 'user') {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+export const CheckInStaffRoute = () => {
+  // const user = useSelector((state: RootState) => state.user.user);
+
+  const user = {
+    role: 'staff'
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (user.role !== 'staff') {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Index
+  },
+  {
+    path: "/how-it-works",
+    Component: HowItWorks
+  },
+  {
+    path: "/about",
+    Component: About
+  },
+  {
+    path: "/contact",
+    Component: Contact
+  },
+  {
+    path: "/legal",
+    Component: Legal
+
+  },
+  {
+    path: "/events",
+    Component: Events
+  },
+  {
+    path: "/events/:id",
+    Component: EventDetail
+  },
+  {
+    path: "/login",
+    Component: Auth
+  },
+
+])
 
 function App() {
-  export const AdminRoute = () => {
-    // const user = useSelector((state: RootState) => state.user.user);
-
-    const user = {
-      role: 'admin'
-    }
-
-    if (!user) {
-      return <Navigate to="/login" />;
-    }
-
-    if (user.role !== 'admin') {
-      return <Navigate to="/login" />;
-    }
-
-    return <Outlet />;
-  };
-
-  export const EventAttendeeRoute = () => {
-    // const user = useSelector((state: RootState) => state.user.user);
-
-    const user = {
-      role: 'user'
-    }
-
-    if (!user) {
-      return <Navigate to="/login" />;
-    }
-
-    if (user.role !== 'user') {
-      return <Navigate to="/login" />;
-    }
-
-    return <Outlet />;
-  };
-
-  export const CheckInStaffRoute = () => {
-    // const user = useSelector((state: RootState) => state.user.user);
-
-    const user = {
-      role: 'staff'
-    }
-
-    if (!user) {
-      return <Navigate to="/login" />;
-    }
-
-    if (user.role !== 'staff') {
-      return <Navigate to="/login" />;
-    }
-
-    return <Outlet />;
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      Component: Index
-    },
-    {
-      path: "/how-it-works",
-      Component: HowItWorks
-    },
-    {
-      path: "/about",
-      Component: About
-    },
-    {
-      path: "/contact",
-      Component: Contact
-    },
-    {
-      path: "/legal",
-      Component: Legal
-
-    },
-    {
-      path: "/events",
-      Component: Events
-    },
-    {
-      path: "/events/:id",
-      Component: EventDetail
-    },
-    {
-      path: "/login",
-      Component: Auth
-    },
-
-  ])
-
   return (
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <RouterProvider router={router} />
