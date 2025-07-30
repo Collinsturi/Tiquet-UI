7fb450b87886c7ea3ce63a9362d5a885d46c59e8
collinsturi <collinsturi@gmail.com>
2025-07-30 09:38:07 +0300

    changed the auth google redirect url

--- src/pages/general/Auth.tsx
+++ src/pages/general/Auth.tsx
@@ -6,7 +6,7 @@ import * as yup from 'yup';
 import { loginSuccess } from '../../queries/general/ApplicationUserSlice.ts';
 
 // Define your backend's base URL
-const BASE_URL = import.meta.env.VITE_BACKEND_API_URL || 'http://localhost:8081/api';
+    const BASE_URL = import.meta.env.VITE_BACKEND_API_URL || 'https://tiquet-ui.vercel.app/';
 
 export const Auth = () => {
     const [isLoginView, setIsLoginView] = useState(true);
